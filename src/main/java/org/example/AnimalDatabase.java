7e3169915bcb52fdacae9adf5ed570245b4125b3
adinazubascu <adina.zubascu@student.upt.ro>
2024-05-19 23:32:15 +0300

    working on the database

--- src/main/java/org/example/AnimalDatabase.java
+++ src/main/java/org/example/AnimalDatabase.java
@@ -1,4 +1,8 @@
+package org.example;
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnimalDatabase {
 
@@ -16,7 +20,6 @@ public class AnimalDatabase {
             connection = DriverManager.getConnection(url, user, password);
             statement = connection.createStatement();
 
-            // Generate random data and insert into the animals table
 
             InsertTestResultsIntoDatabase(name,species,age);
 
@@ -42,4 +45,46 @@ public class AnimalDatabase {
         String query = String.format("INSERT INTO animals(name, species, age) VALUES('%s', '%s','%d')",name,species,age);
         statement.executeUpdate(query);
     }
-}
+    public static List<Animal> getAnimals() {
+        String querySQL = "SELECT name, species , age FROM animals";
+        List<Animal> animalList = new ArrayList<>();
+
+        try {
+            // Establish connection to the database
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+
+             ResultSet rs = statement.executeQuery(querySQL);
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String species= rs.getString("species");
+                int age = rs.getInt("age");
+                Animal animal = new Animal(name,species,age);
+                animalList.add(animal);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return animalList;
+    }
+    public static List<Animal>DeleteAnimalFromAnimalDatabase(String animalName) {
+        String querySQL = "DELETE FROM animals WHERE name = '"+animalName+"'";
+        List<Animal> animalList = new ArrayList<>();
+
+        try {
+            // Establish connection to the database
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+
+            statement.executeUpdate(querySQL);
+            animalList = AnimalDatabase.getAnimals();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return animalList;
+    }
+    }
+
+
